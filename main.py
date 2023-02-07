d032e09c1eda706d331ba1d075cf06de8545bcd9
Shabbir Hasan <shabbir.hasan@outlook.in>
2023-02-07 12:03:48 +0530

    updated

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ except (ImportError, ModuleNotFoundError):
     __import__("os").system(
         f"{__import__('sys').executable} -m pip install -U requests lxml json"
     )
+finally:
     import json
     import requests
     from lxml import html
@@ -76,7 +77,7 @@ def fetchAllowedLists(
 
 
 def updateDict(
-    allowedList: List[str], FinNifty: bool = False
+    allowedList: List[str],
 ) -> Dict[str, Dict[str, Union[List[float], str]]]:
     StrikesAllowed = {}
     if len(allowedList) == 1:
@@ -92,59 +93,37 @@ def updateDict(
                 for _item in item
             ]
         ]
-    if not FinNifty:
-        for item in allowedList:
-            ((nrml, mis,), prd,) = (
-                tuple(
-                    [
-                        item.split(" to ") if " to " in item else item
-                        for item in item.split("NRML:")[-1].split("MIS:")
-                    ]
-                ),
-                item.split("-")[0].strip(),
-            )
-            StrikesAllowed[prd] = {
-                "NRML": (
-                    {"from": float(nrml[0]), "to": float(nrml[-1])}
-                    if isinstance(nrml, list)
-                    else nrml.title()
-                ),
-                "MIS": (
-                    {"from": float(mis[0]), "to": float(mis[-1])}
-                    if isinstance(mis, list)
-                    else mis.title()
-                ),
-            }
-    else:
-        for item in allowedList:
-            prd = item.split("-")[0].strip()
-            prd_data = item.split("-")[-1].strip()
-            nrml = prd_data.split(" to ") if " to " in prd_data else prd_data
-            StrikesAllowed[prd] = {
-                "NRML": (
-                    {"from": float(nrml[0]), "to": float(nrml[-1])}
-                    if isinstance(nrml, list)
-                    else nrml.title()
-                ),
-                "MIS": (
-                    {"from": float(nrml[0]), "to": float(nrml[-1])}
-                    if isinstance(nrml, list)
-                    else nrml.title()
-                ),
-            }
+    for item in allowedList:
+        ((nrml, mis,), prd,) = (
+            tuple(
+                [
+                    item.split(" to ") if " to " in item else item
+                    for item in item.split("NRML:")[-1].split("MIS:")
+                ]
+            ),
+            item.split("-")[0].strip(),
+        )
+        StrikesAllowed[prd] = {
+            "NRML": (
+                {"from": float(nrml[0]), "to": float(nrml[-1])}
+                if isinstance(nrml, list)
+                else nrml.title()
+            ),
+            "MIS": (
+                {"from": float(mis[0]), "to": float(mis[-1])}
+                if isinstance(mis, list)
+                else mis.title()
+            ),
+        }
     return StrikesAllowed
 
 
 if __name__ == "__main__":
-    (
-        bnf_allowed,
-        nf_allowed,
-        fnf_allowed,
-    ) = fetchAllowedLists()
-    StrikesAllowed = {
-        "BankNifty": updateDict(allowedList=bnf_allowed),
-        "Nifty": updateDict(allowedList=nf_allowed),
-        "FinNifty": updateDict(allowedList=fnf_allowed, FinNifty=True),
-    }
+    StrikesAllowed = dict(
+        zip(
+            ("BankNifty", "Nifty", "FinNifty"),
+            tuple(map(updateDict, fetchAllowedLists())),
+        )
+    )
     with open("StrikesAllowed.json", "w") as jsonFile:
         jsonFile.write(json.dumps(StrikesAllowed))
